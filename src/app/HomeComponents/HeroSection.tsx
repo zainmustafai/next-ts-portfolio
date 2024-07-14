af7b0b5a0d5898bc016bec5a7cf4b5190d321c0e
zainmustafai <zforzain2000@gmail.com>
2024-07-14 21:57:47 +0500

    Add Dark Mode

--- src/app/HomeComponents/HeroSection.tsx
+++ src/app/HomeComponents/HeroSection.tsx
@@ -1,19 +1,59 @@
+import { GitHubLogoIcon, InstagramLogoIcon, LinkedInLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons"
+import { FacebookIcon, LucideFacebook } from "lucide-react"
 import Image from "next/image"
+
+const SOCIAL_LINKS = [
+    {
+        name: 'Github', link: 'https://www.github.com/zainmustafai',
+        icon: <GitHubLogoIcon className="w-6 h-6 group-hover:text-primary" />
+    },
+    {
+        name: 'LinkedIn', link: 'https://www.linkedin.com/in/zforzain2000',
+        icon: <LinkedInLogoIcon className="w-6 h-6 group-hover:text-primary" />
+    },
+    {
+        name: 'Twitter', link: 'https://www.x.com/zforzain2000',
+        icon: <TwitterLogoIcon className="w-6 h-6 group-hover:text-primary" />
+    },
+    {
+        name: 'Instagram', link: 'https://www.instagram.com/zforzain2000',
+        icon: <InstagramLogoIcon className="w-6 h-6 group-hover:text-primary" />
+    },
+    {
+        name: 'Facebook', link: 'https://www.facebook.com/zforzain2000',
+        icon: <LucideFacebook className="w-6 h-6 group-hover:text-primary" />
+    },
+]
 
 const HeroSection = () => {
     return (
-        <section className="container min-h-[70dvh]  flex flex-col items-center justify-center">
+        <section className="container min-h-[70dvh]  flex flex-col items-center justify-center text-foreground">
             {/* HERO SECTION */}
-            <div className="w-[200px] h-[200px] rounded-full relative group cursor-pointer ">
+            <div className="w-[200px] h-[200px] rounded-full relative group cursor-pointer z-30">
                 <Image src={"/assets/zain-dp.png"} alt="profile" width={200} height={200} className="rounded-full" loading="lazy" />
                 <div className="absolute size-full inset-0 -z-10 gradient-primary rotate-1 rounded-full -top-1 -left-1 group-hover:-top-2 group-hover:-left-2" />
                 <div className="absolute size-full inset-0 -z-10 gradient-primary rotate-1 rounded-full mt-1 ml-1 group-hover:ml-2 group-hover:mt-2" />
             </div>
             <div className="text-center mt-8">
-                <h2 className="font-bold text-4xl">Hi 👋 I am Zain.</h2>
-                <h1 className="relative max-w-[800px] text-pretty text-2xl md:text-6xl overflow-visible gradient-text py-2 font-bold">
-                    I&apos;m a web developer hailing from 🇵🇰
+                <h2 className="font-bold text-xl md:text-4xl">Hi 👋 I am Zain.</h2>
+                <h1 className="relative max-w-[800px] text-pretty text-2xl md:text-6xl overflow-visible gradient-text py-2 font-black">
+                    I&apos;m a web developer hailing from
+                    <span className="text-foreground">{" "} 🇵🇰</span>
                 </h1>
+            </div>
+
+            <div>
+                {/* SOCIAL LINKS */}
+                <div className="flex justify-center mt-8 space-x-4">
+                    {SOCIAL_LINKS.map((social) => (
+                        <a key={social.name
+                        } href={social.link} target="_blank" rel="noreferrer" className="p-2 rounded-full bg-accent duration-75 group">
+                            {
+                                social.icon
+                            }
+                        </a>
+                    ))}
+                </div>
             </div>
         </section>
     )
--- src/app/HomeComponents/Navbar.tsx
+++ src/app/HomeComponents/Navbar.tsx
@@ -1,14 +1,31 @@
+import { ThemeToggler } from "@/components/shared/ThemeProvider"
+import Image from "next/image"
+import Link from "next/link"
+
+const NAVLINKS = [
+    { name: 'Home', path: '' },
+    { name: 'About', path: 'about' },
+    { name: 'Contact', path: 'contact' },
+    { name: 'Services', path: 'services' },
+    { name: 'Portfolio', path: 'portfolio' },
+    // {name: 'Blog', path: '/blog'}
+]
 
 const Navbar = () => {
     return (
         <div className="h-[100px] border-b border-border w-full">
             <div className="container flex h-full justify-between items-center">
-                <div className="text-2xl font-bold">Logo</div>
-                <div className="flex space-x-4">
-                    <div className="hover:bg-primary">Home</div>
-                    <div>About</div>
-                    <div>Contact</div>
+                <div className="text-2xl font-bold rounded-full">
+                    <Link href="/">
+                        <Image src="/assets/logo.png" alt="logo" width={70} height={70} className="mix-blend-difference" />
+                    </Link>
                 </div>
+                <nav className="hidden space-x-4 md:flex items-center">
+                    {NAVLINKS.map((navlink, index) => (
+                        <Link key={index} href={'#' + navlink.path} className="hover:text-primary text-lg font-semibold">{navlink.name}</Link>
+                    ))}
+                    <ThemeToggler />
+                </nav>
             </div>
         </div>
     )

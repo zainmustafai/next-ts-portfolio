ea65efc07e32611d21a9925569e9cbacb4f27ec0
zainmustafai <zforzain2000@gmail.com>
2024-08-10 11:26:33 +0500

    Testing

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,29 +5,25 @@ const TestPage = async () => {
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjZhZjQ1NzMwNjgwMTdhZWZhNTA0NDY1Iiwicm9sZSI6ImFkbWluIn0sImlhdCI6MTcyMzI2OTU1MiwiZXhwIjoxNzIzMzU1OTUyfQ.ZVa-t9Z_NH-THm_YS4lww3TmnVMifooiNjIZxLnKNv0";
 
     const apiURL = "https://20ca-103-184-92-81.ngrok-free.app";
-    let response: any;
-    let errorInCall: any;
-    try {
-        response = await axios.get(`${apiURL}/api/exercises`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-    } catch (error) {
-        console.clear()
-        if (error instanceof Error) {
-            console.log("EERRRRRORRR", error);
-            errorInCall = error.message;
+
+    const response = await axios.get(`${apiURL}/api/exercises`, {
+        headers: {
+            Authorization: `Bearer ${token}`
         }
-    }
+    }).then((res) => {
+        return res.data;
+    }).catch((error) => {
+        console.log(error);
+    });
     return (
         <div>
             <h1>Test Page</h1>
             <p>This is a test page</p>.
 
+            <hr className='my-20' />
             <pre>
                 {
-                    response ? JSON.stringify(response.data, null, 2) : errorInCall
+                    response ? JSON.stringify(response, null, 2) : "ERROR"
                 }
             </pre>
         </div>

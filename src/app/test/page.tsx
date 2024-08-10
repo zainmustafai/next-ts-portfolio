80ab48b27503d92d8438fc06dbd256651f8e7dda
zainmustafai <zforzain2000@gmail.com>
2024-08-10 11:05:55 +0500

    TESTING

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,15 +1,35 @@
+import axios from 'axios';
 import React from 'react'
 
 const TestPage = async () => {
-    const timeOut = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-    await timeOut(10000);
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjZhZjQ1NzMwNjgwMTdhZWZhNTA0NDY1Iiwicm9sZSI6ImFkbWluIn0sImlhdCI6MTcyMzI2OTU1MiwiZXhwIjoxNzIzMzU1OTUyfQ.ZVa-t9Z_NH-THm_YS4lww3TmnVMifooiNjIZxLnKNv0";
+
+    const apiURL = "https://20ca-103-184-92-81.ngrok-free.app";
+    let response: any;
+    let errorInCall: any;
+    try {
+        response = await axios.get(`${apiURL}/api/exercises`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    } catch (error) {
+        console.clear()
+        if (error instanceof Error) {
+            console.log("EERRRRRORRR", error);
+            errorInCall = error.message;
+        }
+    }
     return (
         <div>
             <h1>Test Page</h1>
             <p>This is a test page</p>.
-            <p>
-                Loaded after 10 seconds.
-            </p>.
+
+            <pre>
+                {
+                    response ? JSON.stringify(response.data, null, 2) : errorInCall
+                }
+            </pre>
         </div>
     )
 }
